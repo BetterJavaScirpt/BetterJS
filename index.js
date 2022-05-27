aa0f39394bd59604fe325d18e4e7d7a97c752c13
BetterJavaScirpt <106377346+BetterJavaScirpt@users.noreply.github.com>
2022-05-27 16:17:23 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// ---------------------------- //
+// Example mineflayer bot code.
+// ---------------------------- //
+
+// Load mineflayer.
+const mineflayer = require('mineflayer')
+
+// Created bot.
+const bot = mineflayer.createBot({
+	host: 'host' // Use only server host! No use letters, letters = error.
+	port: port // User only server port!
+	username: 'username' // Bot username.
+})
+
+// ------------------------------------------------------ //
+// Example mineflayer bot code, made by: BetterJavaScirpt.
+// ------------------------------------------------------ //
+
+// @BetterJavaScirpt
